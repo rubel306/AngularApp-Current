22ee062acd523721b333519273c9bfb5d63c1b05
RubelHasan <rubelhasan306@gmail.com>
2021-01-04 01:17:01 +0600

    Angular File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SupportComponent } from './pages/support/support.component';
+import { ResoursesComponent } from './pages/resourses/resourses.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    SupportComponent,
+    ResoursesComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
